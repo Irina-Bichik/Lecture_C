661d3867b3ba04bb806c8b6f936d9d22a321decf
Irina <fransik@bk.ru>
2023-02-25 18:11:19 +0300

    add array

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -8,7 +8,7 @@ int Max (int arg1, int arg2, int arg3)
     return result;
 }
 
-// //              0     1   2   3   4   5   6   7   8
+// //            0    1   2   3   4   5   6   7   8
 int [] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
 array[0] = 12;
 Console.WriteLine(array[0]);
